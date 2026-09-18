411357a25a72daf0152f2743f71fd4b462bdfea3
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 23:40:42 +0200

    style: format project analysis action result

--- src/types/projectAnalysisActionResult.ts
+++ src/types/projectAnalysisActionResult.ts
@@ -1,3 +1,2 @@
 export type ProjectAnalysisActionResult<T> =
-  | { readonly ok: true; readonly value: T }
-  | { readonly ok: false; readonly error: string };
+  { readonly ok: true; readonly value: T } | { readonly ok: false; readonly error: string };
