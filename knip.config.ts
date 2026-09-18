8ac29c293467128b824644ec0d911f663ad60734
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 07:45:45 +0200

    chore: configure PKGViz Knip entrypoints

--- knip.config.ts
+++ knip.config.ts
@@ -1,3 +1,13 @@
 import { createKnipConfig } from '@ankhorage/devtools/knip';
 
-export default createKnipConfig();
+export default createKnipConfig({
+  entry: [
+    '.prettierrc.js',
+    'eslint.config.mjs',
+    'eslint.examples.config.mjs',
+    'eslint.local.config.mjs',
+    'prettier.local.config.js',
+    'test/**/*.spec.{ts,tsx}',
+  ],
+  ignoreFiles: ['examples/**'],
+});
