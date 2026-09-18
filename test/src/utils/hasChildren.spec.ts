28d128ccae2f07c319219eb658d2c42a0c450a02
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 16:04:48 +0200

    test: use public Testosterone imports

--- test/src/utils/hasChildren.spec.ts
+++ test/src/utils/hasChildren.spec.ts
@@ -1,8 +1,6 @@
+import { describe, expect, it, resolve } from '@artiphishle/testosterone';
 import { hasChildren } from '@/utils/hasChildren';
 import { NodeDefinition } from 'cytoscape';
-import { resolve } from 'node:path';
-import { describe, it } from 'node:test';
-import { expect } from '@artiphishle/testosterone/src/matchers';
 
 describe('[hasChildren]', () => {
   it('returns correct children count of a node', async () => {
