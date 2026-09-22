04aafa9e0f06d9c00841dd5d23585a890099415b
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-22 03:11:52 +0200

    test(project-analysis): remove shared utility test path

--- test/src/shared/utils/getProjectName.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from '@artiphishle/testosterone';
-import { getProjectName } from '@/utils/getProjectName';
-
-describe('[getProjectName]', () => {
-  it('should get the project name from projectPath', () => {
-    process.env.NEXT_PUBLIC_PROJECT_PATH = '/Users/tester/examples/typescript/my-app';
-    expect(getProjectName()).toBe('my-app');
-  });
-});
